a14b99a2256373836a89322fb047144efaaa2aa5
Antony Rizzitelli <rizzitelli.antony@pm.me>
2026-09-03 18:58:19 +0200

    ci: standardize npm releases (#13)
    * ci: standardize npm releases
    
    * style: format release configuration
    
    * test: use current module releases
    
    * fix(ci): pass release arguments correctly

--- src/antelope.test.ts
+++ src/antelope.test.ts
@@ -14,14 +14,14 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/mongodb",
-        version: "1.2.1",
+        version: "1.2.7",
       },
     },
     api: {
       source: {
         type: "package",
         package: "@antelopejs/api",
-        version: "1.0.0",
+        version: "1.2.4",
       },
       config: {
         servers: [{ protocol: "http", host: "127.0.0.1", port: 5010 }],
